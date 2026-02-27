865a59aff8a5582b94f717630bc5d663c0f3106e
Joseph <josehparedes255@gmail.com>
2026-02-27 15:25:14 -0300

    feat(infra): upgrading module compatibility with fargate

--- inputs.tf
+++ inputs.tf
@@ -28,3 +28,38 @@ variable "pollForSourceChanges" {
     description = "Option Codepipeline"
     type = bool
 }
+variable "source_type" {
+    default     = "codecommit"
+    description = "Source provider for the pipeline. Allowed values: 'codecommit', 'github'"
+    type        = string
+
+    validation {
+        condition     = contains(["codecommit", "github"], var.source_type)
+        error_message = "source_type must be 'codecommit' or 'github'."
+    }
+}
+
+variable "github_owner" {
+    default     = ""
+    description = "GitHub organization or user name. Required when source_type = 'github'"
+    type        = string
+}
+
+variable "github_repo" {
+    default     = ""
+    description = "GitHub repository name. Required when source_type = 'github'"
+    type        = string
+}
+
+variable "github_branch" {
+    default     = "main"
+    description = "GitHub branch to track. Used when source_type = 'github'"
+    type        = string
+}
+
+variable "github_oauth_token" {
+    default     = ""
+    description = "GitHub personal access token. Required when source_type = 'github'"
+    type        = string
+    sensitive   = true
+}
--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
-
 resource "aws_s3_bucket" "codepipeline_bucket" {
-    bucket = "codepipeline-bucket-${var.name_main}"
-    # pasarlo a var
+    bucket        = "codepipeline-bucket-${var.name_main}"
     force_destroy = true
 }
 
@@ -13,49 +11,57 @@ resource "aws_s3_bucket_ownership_controls" "codepipeline_ownership_controls" {
 }
 
 resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
-    depends_on = [ aws_s3_bucket_ownership_controls.codepipeline_ownership_controls ]
-    bucket = aws_s3_bucket.codepipeline_bucket.id
-    acl    = "private"
+    depends_on = [aws_s3_bucket_ownership_controls.codepipeline_ownership_controls]
+    bucket     = aws_s3_bucket.codepipeline_bucket.id
+    acl        = "private"
 }
 
+# Solo se crea cuando source_type = "codecommit"
 resource "aws_codecommit_repository" "codecommit_repository" {
+    count           = var.source_type == "codecommit" ? 1 : 0
     repository_name = "Repository_${var.name_main}"
     description     = "This is the project private"
-    default_branch = "main"
+    default_branch  = "main"
     tags = {
         "ENV" = "PROD"
     }
 }
 
+# Credenciales GitHub para CodeBuild - solo cuando source_type = "github"
+resource "aws_codebuild_source_credential" "github" {
+    count       = var.source_type == "github" ? 1 : 0
+    auth_type   = "PERSONAL_ACCESS_TOKEN"
+    server_type = "GITHUB"
+    token       = var.github_oauth_token
+}
 
 data "aws_iam_policy_document" "iam_policy_document_codebuild" {
     statement {
         effect = "Allow"
-        
+
         actions = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
         ]
 
         resources = [
-                "arn:aws:logs:${var.region}:348484763444:log-group:/aws/codebuild/docker-build-${var.name_main}",
-                "arn:aws:logs:${var.region}:348484763444:log-group:/aws/codebuild/docker-build-${var.name_main}:*"
-            ]
-        
+            "arn:aws:logs:${var.region}:${var.account_id}:log-group:/aws/codebuild/docker-build-${var.name_main}",
+            "arn:aws:logs:${var.region}:${var.account_id}:log-group:/aws/codebuild/docker-build-${var.name_main}:*"
+        ]
     }
 
     statement {
         effect = "Allow"
 
         actions = [
-        "ec2:CreateNetworkInterface",
-        "ec2:DescribeDhcpOptions",
-        "ec2:DescribeNetworkInterfaces",
-        "ec2:DeleteNetworkInterface",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeVpcs",
+            "ec2:CreateNetworkInterface",
+            "ec2:DescribeDhcpOptions",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DeleteNetworkInterface",
+            "ec2:DescribeSubnets",
+            "ec2:DescribeSecurityGroups",
+            "ec2:DescribeVpcs",
         ]
 
         resources = ["*"]
@@ -69,7 +75,7 @@ data "aws_iam_policy_document" "iam_policy_document_codebuild" {
         condition {
             test     = "StringEquals"
             variable = "ec2:Subnet"
-            values = var.private_subnets
+            values   = var.private_subnets
         }
 
         condition {
@@ -80,92 +86,88 @@ data "aws_iam_policy_document" "iam_policy_document_codebuild" {
     }
 
     statement {
-        effect  = "Allow"
+        effect = "Allow"
         actions = [
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:GetObjectVersion",
-                "s3:GetBucketAcl",
-                "s3:GetBucketLocation"
-            ]
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:GetObjectVersion",
+            "s3:GetBucketAcl",
+            "s3:GetBucketLocation"
+        ]
         resources = [
             aws_s3_bucket.codepipeline_bucket.arn,
             "${aws_s3_bucket.codepipeline_bucket.arn}/*",
         ]
     }
 
-    statement {
-        effect = "Allow"
-        actions = [
-                "codecommit:GitPull"
-            ]
-        resources = [
-                # "arn:aws:codecommit:us-east-1:348484763444:hudbay"
-                aws_codecommit_repository.codecommit_repository.arn
-            ]
+    # Permiso GitPull de CodeCommit - solo cuando source_type = "codecommit"
+    dynamic "statement" {
+        for_each = var.source_type == "codecommit" ? [1] : []
+        content {
+            effect    = "Allow"
+            actions   = ["codecommit:GitPull"]
+            resources = [aws_codecommit_repository.codecommit_repository[0].arn]
+        }
     }
+
     statement {
         effect = "Allow"
         actions = [
-                "codebuild:CreateReportGroup",
-                "codebuild:CreateReport",
-                "codebuild:UpdateReport",
-                "codebuild:BatchPutTestCases",
-                "codebuild:BatchPutCodeCoverages"
-            ]
+            "codebuild:CreateReportGroup",
+            "codebuild:CreateReport",
+            "codebuild:UpdateReport",
+            "codebuild:BatchPutTestCases",
+            "codebuild:BatchPutCodeCoverages"
+        ]
         resources = [
-            # "arn:aws:codebuild:us-east-1:348484763444:report-group/docker-build-hudbay-dev*"
             "arn:aws:codebuild:${var.region}:${var.account_id}:project/docker-build-${var.name_main}",
             "arn:aws:codebuild:${var.region}:${var.account_id}:project/docker-build-${var.name_main}/*"
         ]
     }
 }
+
 resource "aws_iam_policy" "iam_policy" {
     name        = "CodeBuildBasePolicy-docker-build-${var.name_main}"
     description = "IAM Policy for logs, ec2 and s3"
-    # definirlo en archivo externo
-    policy = data.aws_iam_policy_document.iam_policy_document_codebuild.json
+    policy      = data.aws_iam_policy_document.iam_policy_document_codebuild.json
 }
 
 resource "aws_iam_role" "iam_role" {
     name               = "codebuild-docker-build-service-role-${var.name_main}"
-    # cambiarlo a archivo externo
     assume_role_policy = file("${path.module}/iamPolicies/assume_role_policy.json")
-    managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess", aws_iam_policy.iam_policy.arn]
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_policy" {
+    role       = aws_iam_role.iam_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+    role       = aws_iam_role.iam_role.name
+    policy_arn = aws_iam_policy.iam_policy.arn
 }
 
 resource "aws_codebuild_project" "codebuild_project" {
-    name           = "docker-build-${var.name_main}"
-    description    = "codebuild_project_${var.name_main}"
-    # build_timeout  = 5
-    # queued_timeout = 5
-
+    name         = "docker-build-${var.name_main}"
+    description  = "codebuild_project_${var.name_main}"
     service_role = aws_iam_role.iam_role.arn
 
     source {
-        type            = "CODECOMMIT"
-        # location        = "https://git-codecommit.us-east-1.amazonaws.com/v1/repos/hudbay"
-        location        = aws_codecommit_repository.codecommit_repository.clone_url_http
+        type     = var.source_type == "codecommit" ? "CODECOMMIT" : "GITHUB"
+        location = var.source_type == "codecommit" ? aws_codecommit_repository.codecommit_repository[0].clone_url_http : "https://github.com/${var.github_owner}/${var.github_repo}.git"
         git_clone_depth = 1
     }
 
     artifacts {
-        # type = "NO_ARTIFACTS"
-        name = "build_output"
-        location = aws_s3_bucket.codepipeline_bucket.bucket
-        type = "S3"
-        path = "/"
+        name      = "build_output"
+        location  = aws_s3_bucket.codepipeline_bucket.bucket
+        type      = "S3"
+        path      = "/"
         packaging = "ZIP"
     }
 
-    # cache {
-    #     type  = "LOCAL"
-    #     modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
-    # }
-
     environment {
         compute_type                = "BUILD_GENERAL1_SMALL"
-        
         image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
         type                        = "LINUX_CONTAINER"
         image_pull_credentials_type = "CODEBUILD"
@@ -207,15 +209,13 @@ data "aws_iam_policy_document" "assume_role" {
 
 data "aws_iam_policy_document" "codepipeline_policy" {
     statement {
-        actions = [
-            "iam:PassRole"
-        ]
+        effect    = "Allow"
+        actions   = ["iam:PassRole"]
         resources = ["*"]
-        effect = "Allow"
+
         condition {
             test     = "StringEqualsIfExists"
             variable = "iam:PassedToService"
-
             values = [
                 "cloudformation.amazonaws.com",
                 "elasticbeanstalk.amazonaws.com",
@@ -224,16 +224,15 @@ data "aws_iam_policy_document" "codepipeline_policy" {
             ]
         }
     }
+
     statement {
-        effect = "Allow"  
-        actions = [
-            "ecs:*",
-        ]  
-        resources = ["*"] 
+        effect    = "Allow"
+        actions   = ["ecs:*"]
+        resources = ["*"]
     }
+
     statement {
         effect = "Allow"
-
         actions = [
             "s3:GetObject",
             "s3:GetObjectVersion",
@@ -241,39 +240,37 @@ data "aws_iam_policy_document" "codepipeline_policy" {
             "s3:PutObjectAcl",
             "s3:PutObject",
         ]
-
         resources = [
             aws_s3_bucket.codepipeline_bucket.arn,
             "${aws_s3_bucket.codepipeline_bucket.arn}/*",
         ]
     }
 
-    # statement {
-    #     effect    = "Allow"
-    #     actions   = ["codestar-connections:UseConnection"]
-    #     resources = [aws_codestarconnections_connection.example.arn]
-    # }
-    statement {
-        effect = "Allow"  
-        actions = [
-        "codecommit:CancelUploadArchive",
-        "codecommit:GetBranch",
-        "codecommit:GetCommit",
-        "codecommit:GetUploadArchiveStatus",      
-        "codecommit:UploadArchive"
-        ]  
-        resources = ["*"] 
+    # Permisos CodeCommit para CodePipeline - solo cuando source_type = "codecommit"
+    dynamic "statement" {
+        for_each = var.source_type == "codecommit" ? [1] : []
+        content {
+            effect = "Allow"
+            actions = [
+                "codecommit:CancelUploadArchive",
+                "codecommit:GetBranch",
+                "codecommit:GetCommit",
+                "codecommit:GetUploadArchiveStatus",
+                "codecommit:UploadArchive"
+            ]
+            resources = ["*"]
+        }
     }
+
     statement {
         effect = "Allow"
-
         actions = [
-        "codebuild:BatchGetBuilds",
-        "codebuild:StartBuild",
+            "codebuild:BatchGetBuilds",
+            "codebuild:StartBuild",
         ]
-
         resources = ["*"]
     }
+
     statement {
         effect = "Allow"
         actions = [
@@ -283,11 +280,8 @@ data "aws_iam_policy_document" "codepipeline_policy" {
             "kms:GenerateDataKey*",
             "kms:DescribeKey"
         ]
-        resources = [
-            data.aws_kms_alias.s3kmskey.arn
-        ]
+        resources = [data.aws_kms_alias.s3kmskey.arn]
     }
-
 }
 
 resource "aws_iam_role" "codepipeline_role" {
@@ -308,28 +302,48 @@ resource "aws_codepipeline" "codepipeline" {
     artifact_store {
         location = aws_s3_bucket.codepipeline_bucket.bucket
         type     = "S3"
-
-        # encryption_key {
-        #     id   = data.aws_kms_alias.s3kmskey.arn
-        #     type = "KMS"
-        # }
     }
 
     stage {
         name = "Source"
 
-        action {
-            name             = "Source"
-            category         = "Source"
-            owner            = "AWS"
-            provider         = "CodeCommit"
-            version          = "1"
-            output_artifacts = ["source_output"]
+        # Accion CodeCommit - activa cuando source_type = "codecommit"
+        dynamic "action" {
+            for_each = var.source_type == "codecommit" ? [1] : []
+            content {
+                name             = "Source"
+                category         = "Source"
+                owner            = "AWS"
+                provider         = "CodeCommit"
+                version          = "1"
+                output_artifacts = ["source_output"]
 
-            configuration = {
-                RepositoryName = aws_codecommit_repository.codecommit_repository.repository_name
-                BranchName       = "main"
-                PollForSourceChanges = var.pollForSourceChanges
+                configuration = {
+                    RepositoryName       = aws_codecommit_repository.codecommit_repository[0].repository_name
+                    BranchName           = "main"
+                    PollForSourceChanges = var.pollForSourceChanges
+                }
+            }
+        }
+
+        # Accion GitHub - activa cuando source_type = "github"
+        dynamic "action" {
+            for_each = var.source_type == "github" ? [1] : []
+            content {
+                name             = "Source"
+                category         = "Source"
+                owner            = "ThirdParty"
+                provider         = "GitHub"
+                version          = "1"
+                output_artifacts = ["source_output"]
+
+                configuration = {
+                    Owner                = var.github_owner
+                    Repo                 = var.github_repo
+                    Branch               = var.github_branch
+                    OAuthToken           = var.github_oauth_token
+                    PollForSourceChanges = var.pollForSourceChanges
+                }
             }
         }
     }
@@ -357,19 +371,18 @@ resource "aws_codepipeline" "codepipeline" {
         name = "Deploy"
 
         action {
-        name            = "Deploy"
-        category        = "Deploy"
-        owner           = "AWS"
-        provider        = "ECS"
-        input_artifacts = ["source_output"]
-        version         = "1"
+            name            = "Deploy"
+            category        = "Deploy"
+            owner           = "AWS"
+            provider        = "ECS"
+            input_artifacts = ["source_output"]
+            version         = "1"
 
-        configuration = {
-            ClusterName = var.cluster_name
-            ServiceName = var.service_name
-            FileName    = "imagedefinitions.json"
-            # DeploymentTimeout: "15"
-        }
+            configuration = {
+                ClusterName = var.cluster_name
+                ServiceName = var.service_name
+                FileName    = "imagedefinitions.json"
+            }
         }
     }
 }
